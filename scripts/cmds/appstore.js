7ff2559ed1013b80a636c66e5babcd6dc66bb1ec
kakashihatake420000h-cloud <kakashihatake420000h@gmail.com>
2026-02-21 13:25:45 +0600

    Update appstore.js

--- scripts/cmds/appstore.js
+++ scripts/cmds/appstore.js
@@ -12,7 +12,7 @@ module.exports = {
 			vi: "Tìm app trên appstore",
 			en: "Search app on appstore"
 		},
-		category: "software",
+		category: "Utility",
 		guide: "   {pn}: <keyword>"
 			+ "\n   - Example:"
 			+ "\n   {pn} PUBG",
